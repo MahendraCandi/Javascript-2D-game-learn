40a650724f2e1e110eab1cb679a511ff9f96cbfb
Candi Mahendra <abdumaadjidcandi@gmail.com>
2023-05-06 22:38:35 +0700

    spread sheet

---
+++ src/app.ts
@@ -0,0 +1,117 @@
+// todo: continue tutorial on: https://youtu.be/GFO_txvwK_c?t=2668
+
+// todo: why this script can't use import???
+import * as sprite from "./model/sprite";
+
+console.log("Running the app");
+let testElement = document.getElementById("test")!;
+testElement.innerText = `${new Date()}`; // to test if script is loaded
+
+// start process here...
+let playerState = 'idle';
+let dropDown = <HTMLSelectElement> document.getElementById("animations")!;
+dropDown.addEventListener('change', (evt) => {
+    playerState = (evt.target! as HTMLInputElement).value;
+    // console.log(`${(evt.target! as HTMLInputElement).value}. ${playerState}`)
+})
+
+let canvas = <HTMLCanvasElement> document.getElementById("canvas1")!;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+let ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
+
+const playerImage = new Image();
+playerImage.src = "./img/shadow_dog.png";
+const SPRITE_WIDTH = 575;
+const SPRITE_HEIGHT = 523;
+
+let gameFrame = 0;
+const staggerFrames = 5; // speed of change per frame
+
+let spriteObjects: sprite.spriteType[] = populateSpriteTypes()
+
+// populate the x and y coordinate for each sprite animation
+spriteObjects.forEach((spriteObj, index) => {
+    let coordinate: sprite.coordinate[] = [];
+    for (let j = 0; j < spriteObj.frames; j++) {
+        let positionX = j * SPRITE_WIDTH;
+
+        let positionY = index * SPRITE_HEIGHT;
+        coordinate.push({x: positionX, y:positionY});
+        spriteObj.loc = coordinate;
+    }
+
+});
+// console.log(spriteObjects);
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    let spriteObj = getSpriteObjByName(playerState);
+
+    let position = Math.floor(gameFrame / staggerFrames) % spriteObj.frames;
+    let frameX = SPRITE_WIDTH * position;
+    let frameY = spriteObj.loc![position].y;
+
+    ctx.drawImage(playerImage,
+        frameX, frameY, SPRITE_WIDTH, SPRITE_HEIGHT, // frame in sheet
+        0, 0, SPRITE_WIDTH, SPRITE_HEIGHT // frame in canvas
+    );
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+function populateSpriteTypes() {
+    return [
+        {
+            name: 'idle',
+            frames: 7
+        },
+        {
+            name: 'jump',
+            frames: 7
+        },
+        {
+            name: 'fall',
+            frames: 7
+        },
+        {
+            name: 'run',
+            frames: 9
+        },
+        {
+            name: 'dizzy',
+            frames: 11
+        },
+        {
+            name: 'sit',
+            frames: 5
+        },
+        {
+            name: 'roll',
+            frames: 7
+        },
+        {
+            name: 'bite',
+            frames: 7
+        },
+        {
+            name: 'ko',
+            frames: 12
+        },
+        {
+            name: 'getHit',
+            frames: 4
+        },
+    ];
+}
+
+function getSpriteObjByName(name: string = 'idle') {
+    return spriteObjects.find((value, index, array): sprite.spriteType | null => {
+        if (name == value.name) return value;
+        return null;
+    })!;
+}
+
+animate();
---
+++ src/model/sprite.ts
@@ -0,0 +1,2 @@
+export type coordinate = { x: number, y: number };
+export type spriteType = {name: string, frames: number, loc?: coordinate[]}
